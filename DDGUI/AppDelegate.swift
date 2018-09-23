4d99c925075a38b4e93ec582ed748276f2d1125d
Santeri Hetekivi <santeri@hetekivi.com>
2018-09-23 13:08:35 +0300

    Initial Commit

---
+++ DDGUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DDGUI
+//
+//  Created by Santeri Hetekivi on 23/09/2018.
+//  Copyright © 2018 Santeri Hetekivi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DDGUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DDGUI
+//
+//  Created by Santeri Hetekivi on 23/09/2018.
+//  Copyright © 2018 Santeri Hetekivi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
